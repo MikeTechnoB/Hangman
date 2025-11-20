289dea5a80a521fd917336e4e6fadc7ff5eef4da
MikeTechnoB <margulan.ye@gmail.com>
2025-11-20 21:33:58 +0000

    converted to React code

--- hangman-react/src/App.jsx
+++ hangman-react/src/App.jsx
@@ -1,50 +1,101 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const ALPHABET = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
 const WORDOFTHEDAY = "ABOUT";
+
 function App() {
+  const [lives, setLives] = useState(5);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const handleLetterClick = (letter) => {
+    console.log("Clicked:", letter);
+    setGuessedLetters([...guessedLetters, letter]);
+
+    if (!WORDOFTHEDAY.includes(letter)) {
+      setLives(lives - 1);
+    }
+  };
+
+  const handleGameOver = () => {
+    alert("You Lose! Word is:" + WORDOFTHEDAY);
+    setLives(5);
+    setGuessedLetters([]);
+  };
+
+  const handleWin = () => {
+    alert("You won!");
+    setLives(5);
+    setGuessedLetters([]);
+  };
+
+  useEffect(() => {
+    const checkAllLettersGuessed = WORDOFTHEDAY.split("").every((letter) =>
+      guessedLetters.includes(letter)
+    );
+
+    if (checkAllLettersGuessed && guessedLetters.length > 0) {
+      setTimeout(() => {
+        handleWin();
+      }, 500);
+    }
+    if (lives === 0) {
+      setTimeout(() => {
+        handleGameOver();
+      }, 500);
+    }
+  }, [guessedLetters, lives]);
+
   return (
     <main>
       <div className="WOD">
         {WORDOFTHEDAY.split("").map((letterOfWORD) => (
-          <div className="lettersWORD"></div>
+          <div className="lettersWORD">
+            {guessedLetters.includes(letterOfWORD) ? letterOfWORD : ""}
+          </div>
         ))}
       </div>
 
       {ALPHABET.map((letter) => (
-        <button id={letter} className="button-letters">
+        <button
+          id={letter}
+          className="button-letters"
+          onClick={() => handleLetterClick(letter)}
+          disabled={guessedLetters.includes(letter)}
+        >
           {letter}
         </button>
       ))}
+
+      <div className="liveleft">Live Left: {lives}</div>
     </main>
   );
 }

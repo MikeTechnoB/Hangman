869f3092b36d032e69545c6d138b4320ff56bf42
MikeTechnoB <margulan.ye@gmail.com>
2025-11-22 18:19:39 +0000

    added word library and word defination fetch

--- hangman-react/src/App.jsx
+++ hangman-react/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import WORDS from "./words.js";
+import confetti from "canvas-confetti";
 
 const ALPHABET = [
   "A",
@@ -30,23 +32,34 @@ const ALPHABET = [
   "Z",
 ];
 
-const WORDOFTHEDAY = "ABOUT";
+const RANDOMWORD = WORDS[Math.floor(Math.random() * WORDS.length)];
 const WORDOFTHEDAY_URL = "https://word-of-the-day.de/api/random";
+const WORDOFTHEDAY_Def_URL = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 
 function App() {
-  const [lives, setLives] = useState(5);
+  const [DEF_OFWORD, setDefOfWord] = useState("");
+  const [lives, setLives] = useState(8);
   const [guessedLetters, setGuessedLetters] = useState([]);
+  const [WORDOFTHEDAY, setWordOfTheDay] = useState(RANDOMWORD);
 
+  // couldn't find a random word from the api
   async function fetchWordOfTheDay() {
-    const wordRes = await fetch("WORDOFTHEDAY_URL");
-    console.log(wordRes);
+    const wordRes = await fetch(WORDOFTHEDAY_URL);
+    console.log();
     const wordJson = await wordRes.json();
-    console.log(wordJson);
+  }
+
+  async function fetchWordOfTheDayMeaning(WORDOFTHEDAY) {
+    const wordRes = await fetch(WORDOFTHEDAY_Def_URL + WORDOFTHEDAY);
+    const wordJson = await wordRes.json();
+    let DEF_OFWORD = wordJson[0].meanings[0].definitions[0].definition;
+    setDefOfWord(DEF_OFWORD);
   }
 
   useEffect(() => {
-    fetchWordOfTheDay();
-  }, []);
+    //fetchWordOfTheDay();
+    fetchWordOfTheDayMeaning(WORDOFTHEDAY);
+  }, [WORDOFTHEDAY]);
 
   const handleLetterClick = (letter) => {
     console.log("Clicked:", letter);
@@ -59,12 +72,21 @@ function App() {
 
   const handleGameOver = () => {
     alert("You Lose! Word is:" + WORDOFTHEDAY);
+    setWordOfTheDay(WORDS[Math.floor(Math.random() * WORDS.length)]);
     setLives(5);
     setGuessedLetters([]);
   };
 
   const handleWin = () => {
+    // Trigger confetti
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+
     alert("You won!");
+    setWordOfTheDay(WORDS[Math.floor(Math.random() * WORDS.length)]);
     setLives(5);
     setGuessedLetters([]);
   };
@@ -92,7 +114,7 @@ function App() {
         <img src="src/assets/hangman.png" alt="" />
         HANGMAN
       </header>
-      <div className="wordhint">()</div>
+      <div className="wordhint">({DEF_OFWORD})</div>
       <main>
         <div className="WOD">
           {WORDOFTHEDAY.split("").map((letterOfWORD) => (
